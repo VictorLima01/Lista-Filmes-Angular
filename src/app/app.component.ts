fc2d259c513c4aff58af27539db10381af78217e
VictorLima01 <victor.lima.araujo@gmail.com>
2021-04-09 18:30:03 -0300

    Ciando componente de listagem de filmes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as data from './filmes.json';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.filmes = data;
+    console.log(data)
+  }
   title = 'filmes';
+  public filmes: any;
 }
