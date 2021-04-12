e353a3209f1e7c8d9c8417830bca675f47dc891a
VictorLima01 <victor.lima.araujo@gmail.com>
2021-04-11 23:42:44 -0300

    Adicionando Rotas das paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from '../app/app.component';
+import { LocadoraComponent } from '../app/locadora/locadora.component';
+import { ListaFilmesComponent } from '../app/lista-filmes/lista-filmes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'lista', component: ListaFilmesComponent },
+  { path: 'locadora', component: LocadoraComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
