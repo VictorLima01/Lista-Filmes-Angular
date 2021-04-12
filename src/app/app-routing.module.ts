c453f36d47b23691c26ae9bab164284571e5e18f
VictorLima01 <victor.lima.araujo@gmail.com>
2021-04-12 18:28:26 -0300

    Adição de novo item de menu de grid, navegavel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from '../app/app.component';
 import { LocadoraComponent } from '../app/locadora/locadora.component';
 import { ListaFilmesComponent } from '../app/lista-filmes/lista-filmes.component';
+import { GridCssComponent } from '../app/grid-css/grid-css.component';
 
 const routes: Routes = [
   { path: 'lista', component: ListaFilmesComponent },
   { path: 'locadora', component: LocadoraComponent },
+  { path: 'grid-css', component: GridCssComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { LocadoraComponent } from './locadora/locadora.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ListaFilmesComponent } from './lista-filmes/lista-filmes.component';
 import { SafeUrlPipe } from './pipes/safe-url.pipe';
+import { GridCssComponent } from './grid-css/grid-css.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LocadoraComponent,
     ListaFilmesComponent,
-    SafeUrlPipe
+    SafeUrlPipe,
+    GridCssComponent
   ],
   imports: [
     BrowserModule,
