73892e46c907225fcb1aa3e4e2ae0dd2a5d3096e
VictorLima01 <victor.lima.araujo@gmail.com>
2021-04-11 23:43:25 -0300

    Declarando e importando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocadoraComponent } from './locadora/locadora.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ListaFilmesComponent } from './lista-filmes/lista-filmes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocadoraComponent,
+    ListaFilmesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
