96c6be87027c2dfa3a6c198a36cd91129af88ff6
VictorLima01 <victor.lima.araujo@gmail.com>
2021-04-11 23:41:57 -0300

    Adicionando componente de Locadora para o outro teste

---
+++ src/app/locadora/locadora.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LocadoraComponent } from './locadora.component';
+
+describe('LocadoraComponent', () => {
+  let component: LocadoraComponent;
+  let fixture: ComponentFixture<LocadoraComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LocadoraComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LocadoraComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/locadora/locadora.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-locadora',
+  templateUrl: './locadora.component.html',
+  styleUrls: ['./locadora.component.scss']
+})
+export class LocadoraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
