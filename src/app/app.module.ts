71d8e3bf687f89cb55ddf7a283013ebd6bede3e4
VictorLima01 <victor.lima.araujo@gmail.com>
2021-04-13 17:58:20 -0300

    Import de bibliotecas de form, marcara e etc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule, FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LocadoraComponent } from './locadora/locadora.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ListaFilmesComponent } from './lista-filmes/lista-filmes.component';
+/*
+  Para aceitar url unsafed
+*/
 import { SafeUrlPipe } from './pipes/safe-url.pipe';
 import { GridCssComponent } from './grid-css/grid-css.component';
 import { HeaderComponent } from './header/header.component';
+
+/*
+Mascará
+*/
+import { NgxMaskModule } from 'ngx-mask';
+import { CurrencyMaskModule } from "ng2-currency-mask";
+
+/*
+  Toast
+*/
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +38,13 @@ import { HeaderComponent } from './header/header.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxMaskModule.forRoot(),
+    CurrencyMaskModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
